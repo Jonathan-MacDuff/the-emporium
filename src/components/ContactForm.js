a0481318218346e2930fb5e2ade1eda5a89d865e
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-03-30 13:54:52 -0700

    added spacing to css and removed unnecesssary br elements from Contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -25,23 +25,14 @@ function ContactForm() {
         <form id="contactForm">
             <h1>Contact Form</h1>
             <label htmlFor="firstName">First Name: </label>
-            <br/>
             <input id="firstName" value={contactInfo.firstName} onChange={onFieldChange}></input>
-            <br/>
             <label htmlFor="lastName">Last Name: </label>
-            <br/>
             <input id="lastName" value={contactInfo.lastName} onChange={onFieldChange}></input>
-            <br/>
             <label htmlFor="phone">Phone: </label>
-            <br/>
             <input id="phone" value={contactInfo.phone} onChange={onFieldChange}></input>
-            <br/>
             <label htmlFor="email">Email: </label>
-            <br/>
             <input id="email" value={contactInfo.email} onChange={onFieldChange}></input>
-            <br/>
             <label htmlFor="message">Message: </label>
-            <br/>
             <input id="message" value={contactInfo.message} onChange={onFieldChange}></input>
             <br/>
             <button>Submit</button>
