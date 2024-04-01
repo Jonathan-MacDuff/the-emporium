0032c1495582079e6014a084fbea968b5e93ae66
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-03-31 21:42:57 -0700

    added homepage navigation panels, styling needed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
+import { NavLink } from "react-router-dom";
+
 function Home() {
     return (
         <main>
-            <p>Home</p>
+            <h1 id="homeHeader">Welcome</h1>
+            <div id="homePanelContainer">                
+                <NavLink 
+                className="homePanel" 
+                id="aboutPanel" 
+                to="/about"
+                >About</NavLink>
+                
+                <NavLink 
+                className="homePanel" 
+                id="contactPanel" 
+                to="/contact"
+                >Contact</NavLink>
+                
+                <NavLink 
+                className="homePanel" 
+                id="musingsPanel" 
+                to="/musings"
+                >Musings</NavLink>
+                               
+                <NavLink 
+                className="homePanel" 
+                id="workPanel" 
+                to="/work"
+                >Work</NavLink>                
+            </div>
         </main>
     )
 }
